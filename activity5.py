f2534370659c5253b3c1d0fe3c9d505a61ab908c
SChemist23 <bent0272@bears.unco.edu>
2025-10-21 03:15:50 +0000

    testacticity5

--- activity5.py
+++ activity5.py
@@ -1,14 +1,37 @@
-### more than 20
-def more_than_20(file):
-    words = []
-    data = open(file, 'r')
-    for word in data:
-        if len(word.strip()) > 20:
-            words.append(word.strip())
-##different style, same result.
-words = [x.strip() for x in data if len(x.strip()) > 20]
-
-print(more_than_20("CROSSWD.txt"))
+def twenty_or_more(CROSSWD.txt):
+    long_words = []                        # list to store long words
+    with open(CROSSWD.txt, 'r') as f:             # open the file for reading
+        for line in f:                     # read each line
+            words = line.split()           # split the line into words
+            for word in words:
+                if len(word) > 20:         # check word length
+                    long_words.append(word)  # add to list
+    return long_words
 
 
 
+def has_no_e(word):
+    for letter in word:
+        if letter == 'e' or letter == 'E':
+            return False
+    return True
+
+
+
+def uses_only(word, letters):
+    for letter in word:
+        if letter not in letters:
+            return False
+    return True
+
+
+
+def all_uses_only(file, letters):
+    valid_words = []
+    with open(file, 'r') as f:
+        for line in f:
+            words = line.split()
+            for word in words:
+                if uses_only(word, letters):    
+                    valid_words.append(word)
+    return valid_words
