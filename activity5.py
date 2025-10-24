9d2d92ea8482f7c4edb99fd2a1ed326f24f05977
SChemist23 <bent0272@bears.unco.edu>
2025-10-24 01:30:59 +0000

    activity5

--- activity5.py
+++ activity5.py
@@ -1,9 +1,9 @@
 def more_than_20(file):
     words = []    
     data = open(file, 'r') 
-    for word in data:
-        if len(word.strip()) > 20:
-            words.append(word.strip())
+    #for word in data:
+        #if len(word.strip()) > 20:
+            #words.append(word.strip())
     words = [x.strip() for x in data if len (x.strip()) > 20]
     return words
 
